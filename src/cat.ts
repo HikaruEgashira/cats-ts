1a5d97ed1ae9946887603b466135d66a9d36ee5a
HikaruEgashira <shighten@gmail.com>
2021-05-03 22:05:04 +0900

    impl cat

---
+++ src/cat.ts
@@ -0,0 +1,24 @@
+import { Printable, printable, printableInstances } from './printable';
+
+export class Cat {
+  name: string;
+  age: number;
+  color: string;
+
+  constructor(name: string, age: number, color: string) {
+    this.name = name;
+    this.age = age;
+    this.color = color;
+  }
+}
+
+export const catPrintable: Printable<Cat> = {
+  format: (cat: Cat) => {
+    const name = printable.format(cat.name)(printableInstances.stringPrintable);
+    const age = printable.format(cat.age)(printableInstances.numberPrintable);
+    const color = printable.format(cat.color)(
+      printableInstances.stringPrintable
+    );
+    return `${name} is a ${age} year-old ${color} cat.`;
+  },
+};
